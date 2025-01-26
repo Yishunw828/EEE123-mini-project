54e287a084e2ceb35ddc7bb78a6001b388fd9479
Yishunw828 <yishunw828@student.usm.my>
2025-01-26 22:19:06 +0800

    done

--- search.hpp
+++ search.hpp
@@ -1,13 +1,15 @@
 #ifndef SEARCH_HPP
 #define SEARCH_HPP
 
+using namespace std;
+
 // Programmers: Divyadarrshini A/P Loganathan
 // Matric No. : 23303060
 // Purpose: Declare functions for searching patient records by name, ward number or admission date.
 void searchMenu();
 void searchByName();
 void searchByWard();
-bool ValidDate(const tring& date);
+bool ValidDate(const string& date);
 void searchByDate();
 
 #endif
